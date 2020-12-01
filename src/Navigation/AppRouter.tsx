7cccc283a805dcbfe00854b65b08d363c349bf08
Manish <manish@mobilecoderz.com>
2020-12-01 13:11:23 +0530

    splashscreen setup

--- src/Navigation/AppRouter.tsx
+++ src/Navigation/AppRouter.tsx
@@ -1,18 +1,21 @@
 import AppStateHandler, {useAppState} from 'src/StateHandlers/AppStateHandler';
-import React, {useLayoutEffect} from 'react';
-
+import React, {useEffect, useLayoutEffect} from 'react';
 import {ActivityIndicator} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStackNavigator from './StackNavigators/AuthStackNavigator';
 import RootStackNavigator from './StackNavigators/RootStackNavigator';
 import Center from 'src/Components/Shared/Center/Center';
 import Container from 'src/Components/Shared/Container/Container';
+import SplashScreen from 'react-native-splash-screen';
 
 function AppRouter() {
   const [{loggedIn, loading}] = useAppState(['loggedIn', 'loading']);
 
   useLayoutEffect(() => {
-    AppStateHandler.checkLogin();
+    SplashScreen.hide();
+    setTimeout(() => {
+      AppStateHandler.setState({loading: false});
+    }, 2000);
   }, []);
 
   if (loading) {
